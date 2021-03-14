322cc80adbd498ab7ece2a4bd98340b1a82991c4
sneakers-the-rat <JLSaunders987@gmail.com>
2021-03-13 23:13:56 -0800

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,9 @@ author = 'sneakers-the-rat'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
